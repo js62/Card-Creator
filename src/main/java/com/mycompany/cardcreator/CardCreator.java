38cfad34a265195a06bf3116cf53a57339170643
Joel <joeladamsch@gmail.com>
2026-01-21 16:35:44 -0600

    Initial commit

---
+++ src/main/java/com/mycompany/cardcreator/CardCreator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cardcreator;
+
+/**
+ *
+ * @author jas
+ */
+public class CardCreator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
